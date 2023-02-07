fbb1268413015b6d90d832223e1bd4d87e4c91c2
DogukanPolatel <70372233+dogukan1047@users.noreply.github.com>
2023-02-07 11:42:09 +0300

    Again1

---
+++ Strings_Integer_Float_Type/Main.py
@@ -0,0 +1,23 @@
+name="Dogukan"
+surName="Polatel"
+message="message"
+print(name[::-1]) #reverse
+print(surName[::2])
+
+print(message.capitalize())#Bas harfi büyütür
+print(message.startswith("me"))
+print(message.endswith("e"))
+print("dogukan"*10)
+print(f" {name} ,{surName}")
+
+
+number1=15
+number2=3.9
+number3=2
+number4=4
+
+print(type(number1),type(number2))
+print(number4**number3)
+print(number1//number3)
+print(abs(-8))
+print(round(3.8744,2))
